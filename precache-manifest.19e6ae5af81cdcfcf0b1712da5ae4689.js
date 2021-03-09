0de923a4e3f97e9ca99ff3194bed700ac47d56e7
corollari <corollari@users.noreply.github.com>
2021-03-09 19:33:21 +0000

    deploy: 7939387e287f5d8caccc4e66cd7599cf00565b5c

---
+++ precache-manifest.19e6ae5af81cdcfcf0b1712da5ae4689.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60463b4c1676720ffd23c1a8a9446212",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99edbdc8099f00e3bc11",
+    "url": "/static/css/main.a2346393.chunk.css"
+  },
+  {
+    "revision": "2db49e24f617e9fe6df6",
+    "url": "/static/js/2.cb8ced34.chunk.js"
+  },
+  {
+    "revision": "bd32164b0b1f07ae79545cc1dff28ec5",
+    "url": "/static/js/2.cb8ced34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99edbdc8099f00e3bc11",
+    "url": "/static/js/main.abe49660.chunk.js"
+  },
+  {
+    "revision": "aa6f0c87fa9ca1c20304",
+    "url": "/static/js/runtime-main.fba19f84.js"
+  },
+  {
+    "revision": "00e2257b7bacfa1287014ba0a5cd6820",
+    "url": "/static/media/icons.00e2257b.svg"
+  },
+  {
+    "revision": "0967e45f1013e43679bd4b14fef05ac4",
+    "url": "/static/media/loader.0967e45f.svg"
+  }
+]);
