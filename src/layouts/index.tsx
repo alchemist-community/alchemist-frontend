a5f9802d80885b95783242a8072b21feca9572dd
nuurcodes <nuurcodes@gmail.com>
2021-03-16 22:31:31 +0000

    Added pool image

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -3,7 +3,9 @@ import Footer from "../views/Footer";
 import Header from "../views/Header";
 import Body from "../views/Body";
 import bg from '../img/bg.jpg'
+import pool from '../img/pool.png'
 import React from "react";
+import { Image } from "@chakra-ui/image";
 
 interface LayoutProps {}
 
@@ -16,8 +18,21 @@ const Layout: React.FC<LayoutProps> = () => {
       flexDirection="column"
       backgroundSize="cover"
       backgroundPosition="center"
+      backgroundRepeat="no-repeat"
       background={`url(${bg})`}
+      position="relative"
+      overflow="hidden"
+      zIndex={0}
     >
+      <Image
+        src={pool}
+        position="absolute"
+        backgroundPosition="center"
+        display={['none', 'none', 'block']}
+        bottom={-200}
+        zIndex={-1}
+        left={0}
+      />
       <Header />
       <Box flexGrow={1}>
         <Body />
