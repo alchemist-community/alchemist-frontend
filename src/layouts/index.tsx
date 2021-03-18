56d4d599b3876113c6a9574a70bd10caff8ebe77
Matthew Nevle <Manevle16@gmail.com>
2021-03-18 17:24:00 -0500

    change connect wallet styling

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -2,18 +2,16 @@ import { Box, Flex } from "@chakra-ui/layout";
 import Footer from "../views/Footer";
 import Header from "../views/Header";
 import Body from "../views/Body";
-import bg from '../img/bg.jpg'
-import pool from '../img/pool.png'
+import bg from "../img/bg.jpg";
+import pool from "../img/pool.png";
 import React from "react";
 import { Image } from "@chakra-ui/image";
 
 interface LayoutProps {}
 
 const Layout: React.FC<LayoutProps> = () => {
-
   return (
     <Flex
-      px={[4, 4, 12]}
       minHeight="100vh"
       flexDirection="column"
       backgroundSize="cover"
@@ -28,7 +26,7 @@ const Layout: React.FC<LayoutProps> = () => {
         src={pool}
         position="absolute"
         backgroundPosition="center"
-        display={['none', 'none', 'block']}
+        display={["none", "none", "block"]}
         bottom={-200}
         zIndex={-1}
         left={0}
