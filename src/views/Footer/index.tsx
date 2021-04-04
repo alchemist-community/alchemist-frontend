1f29ac384baa6b61d411c63a79ae932a6a438b7a
nuurcodes <nuurcodes@gmail.com>
2021-04-04 12:44:33 +0100

    Update footer

--- src/views/Footer/index.tsx
+++ src/views/Footer/index.tsx
@@ -1,24 +1,35 @@
-import { Box, Flex, HStack, Link, Text } from "@chakra-ui/layout";
+import { IconButton } from "@chakra-ui/button";
+import { Center, HStack, Link } from "@chakra-ui/layout";
 import React from "react";
+import {
+  FaDiscord,
+  FaGithub,
+  FaQuestionCircle,
+  FaVoteYea,
+} from "react-icons/fa";
 
 const links = [
   {
-    label: "Code",
-    href: "https://github.com/alchemist-community/alchemist-frontend",
-    isExternal: true,
-  },
-  {
     label: "Discord",
+    icon: <FaDiscord />,
     href: "http://discord.alchemist.wtf",
     isExternal: true,
   },
   {
+    label: "Code",
+    icon: <FaGithub />,
+    href: "https://github.com/alchemist-community/alchemist-frontend",
+    isExternal: true,
+  },
+  {
     label: "FAQ",
+    icon: <FaQuestionCircle />,
     href: "https://hackmd.io/@thegostep/BJ40PSVQd",
     isExternal: true,
   },
   {
     label: "Governance",
+    icon: <FaVoteYea />,
     href: "https://cast.alchemist.wtf",
     isExternal: true,
   },
@@ -26,45 +37,36 @@ const links = [
 
 const Footer: React.FC = () => {
   return (
-    <Box as="footer" px={[4, 4, 12]}>
-      <Flex
-        py={8}
-        flexDirection={["column", "column", "row"]}
-        justifyContent="space-between"
-        alignItems="center"
-        as="footer"
-      >
-        <Box textAlign={["center", "center", "initial"]}>
-          <Text fontWeight="bold" fontSize="md">
-            alchemist.farm
-          </Text>
-          <Text fontSize="sm" color="gray.200">
-            Searching for the philosopher's stone
-          </Text>
-        </Box>
-        <Box>
-          <HStack spacing={4}>
-            {links.map(({ href, isExternal, label }) => (
-              <Link
-                py={1}
-                key={label}
-                href={href}
-                fontSize="sm"
-                isExternal={isExternal}
-                rel="noopener noreferrer"
-                _hover={{
-                  color: "brand.400",
-                  borderBottomColor: "brand.400",
-                  borderBottomWidth: 1,
-                }}
-              >
-                {label}
-              </Link>
-            ))}
-          </HStack>
-        </Box>
-      </Flex>
-    </Box>
+    <Center py={12}>
+      <HStack spacing={4}>
+        {links.map(({ href, isExternal, label, icon }) => (
+          <Link
+            py={1}
+            key={label}
+            href={href}
+            fontSize="sm"
+            isExternal={isExternal}
+            rel="noopener noreferrer"
+            _hover={{
+              color: "white",
+            }}
+          >
+            <IconButton
+              size="lg"
+              isRound
+              fontSize="2xl"
+              aria-label={label}
+              icon={icon}
+              _hover={{
+                color: "gray.800",
+                background: "#FFBF00",
+                boxShadow: "4px 4px 4px solid yellow",
+              }}
+            />
+          </Link>
+        ))}
+      </HStack>
+    </Center>
   );
 };
 
