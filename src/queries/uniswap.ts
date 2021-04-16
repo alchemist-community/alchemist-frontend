cb1520c44f1108fe8e409b82fca7e3e02f6951a9
Parker Place <pleeplace@gmail.com>
2021-04-15 22:33:01 -0400

    Merge in LP stats (#76)
    * Bump TS version
    
    * Update build script (#50)
    
    * we need to check against the wallet address not the crucible address
    
    * check needs to be against the wallet
    
    * center chakra image (#52)
    
    * Revert add stake changes (#54)
    
    * check against wallet (#53)
    
    * Fix bg placement (#57)
    
    * Fix prod deployments (#58)
    
    * Added Crucible LP MIST and ETH balances and dollar amounts (#55)
    
    * add arg
    
    * add uniswap amounts and balances
    
    * Fix increase stake functionality (#56)
    
    * add usd to rewards
    
    * Update github actions whitespace
    
    Co-authored-by: nuurcodes <71561866+nuurcodes@users.noreply.github.com>
    Co-authored-by: nuurcodes <nuurcodes@gmail.com>
    
    * Added support for rinkeby network (#59)
    
    * Allow unstake on rinkeby (#63)
    
    * Set dark color mode (#64)
    
    * Design cleanup (#66)
    
    * Display uniswap deposits, gains/loss from LP, historical timestamps (#61)
    
    * add arg
    
    * add uniswap amounts and balances
    
    * add usd to rewards
    
    * Update github actions whitespace
    
    * show lp stats
    
    * wip get token prices at mint timestamp
    
    * hardcode timestamp fallback for testing
    
    * fix dependent graphql queries, add cummuulative gains, add tooltips
    
    * format
    
    * add more width
    
    * Color mode (#65)
    
    * added index prop for multiple crucibles
    
    Co-authored-by: nuurcodes <71561866+nuurcodes@users.noreply.github.com>
    Co-authored-by: nuurcodes <nuurcodes@gmail.com>
    
    * Update package lock
    
    * Change ssh to https
    
    * Fix mint date (#68)
    
    * add arg
    
    * add uniswap amounts and balances
    
    * Display uniswap stats (#67)
    
    * fix date
    
    * remove merge conflict
    
    * fix closing bracket
    
    * remove random s
    
    * Update index.tsx
    
    * hide rewards for now (#70)
    
    * Copy changes (#72)
    
    * New copy (#74)
    
    * Add per crucible stats (#71)
    
    * wip mint pairs history
    
    * fix time-based lookup
    
    * working
    
    * add mainnet check to subgraph call
    
    * nan checks
    
    * fix for multiple crucibles
    
    * copy changes
    
    Co-authored-by: nuurcodes <nuurcodes@gmail.com>
    Co-authored-by: nuurcodes <71561866+nuurcodes@users.noreply.github.com>
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-17-34.us-east-2.compute.internal>
    Co-authored-by: sal-ash <s.el-ashaal@live.co.uk>
    Co-authored-by: Rove Mind <34493975+rovemind@users.noreply.github.com>
    Co-authored-by: sal-ash <44524714+sal-ash@users.noreply.github.com>

--- src/queries/uniswap.ts
+++ src/queries/uniswap.ts
@@ -59,3 +59,109 @@ export const GET_PRICES = gql`
     }
   }
 `;
+// gql`
+// query getPrices{
+//       pairHourDatas(
+//         where: {
+//           pair: 0xcd6bcca48069f8588780dfa274960f15685aee0e
+//           hourStartUnix_lt: 1613653200
+//           hourStartUnix_gt: 1613649600
+//         }
+//       ) {
+//         id
+//         reserve0
+//         reserve1
+//         hourStartUnix
+//         reserveUSD
+//       }
+//     )
+// `
+
+export const createPairHistoryQuery = (
+  address: string,
+  timestamps: number[]
+) => {
+  let queryString = ``;
+  timestamps.map((timestamp, i) => {
+    queryString += `
+      pairHour${i}: pairHourDatas(
+        where: {
+          pair: "${address}"
+          hourStartUnix_lte: ${timestamp}
+          hourStartUnix_gte: ${timestamp - 3600}
+        }
+      ) {
+        id
+        reserve0
+        reserve1
+        hourStartUnix
+        reserveUSD
+        pair {
+          totalSupply
+        }
+      }
+      pairDay${i}: pairDayDatas(
+        where: {
+          pairAddress:"0xcd6bcca48069f8588780dfa274960f15685aee0e", 
+          date_lte:${timestamp}
+          date_gte:${timestamp - 3600 * 24} 
+        }){
+          totalSupply
+          reserve0
+          reserve1
+      }
+    `;
+  });
+  let wrappedQuery = `query getPrices{${queryString}}`;
+  console.log(wrappedQuery);
+  return gql`
+    ${wrappedQuery}
+  `;
+};
+
+// {
+//   liquidityPositions(id: "0x365595AB460cB664c77d4e038c9051f09D781065-0xcd6bcca48069f8588780dfa274960f15685aee0e") {
+//     id
+//     user{
+//       id
+//     }
+//     liquidityTokenBalance
+//   }
+// }
+
+// {
+//   pairDayDatas (where: {pairAddress: "0xcd6bcca48069f8588780dfa274960f15685aee0e", }){
+//     id
+//     pairAddress
+//     reserve0
+//     reserve1
+//     reserveUSD
+//     totalSupply
+//     token0{
+//       id
+//     }
+//     token1{
+//       id
+//     }
+//   }
+// }
+
+// pairHourDatas(where:{pair:"0xcd6bcca48069f8588780dfa274960f15685aee0e"} ){
+//   id
+//   reserve0
+//   reserve1
+//   hourStartUnix
+//   reserveUSD
+// }
+
+// pairHourDatas(where:{
+//   pair:"0xcd6bcca48069f8588780dfa274960f15685aee0e"
+//   hourStartUnix_gt: 1613653200
+//   hourStartUnix_lt: 1613658200
+// } ){
+//   id
+//   reserve0
+//   reserve1
+//   hourStartUnix
+//   reserveUSD
+// }
